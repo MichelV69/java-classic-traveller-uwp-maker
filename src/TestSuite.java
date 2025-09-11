bc1559281c06da781d96370234d4b4fedce20143
Michel @ Mac Mini Intel <michel@wolfstar.ca>
2025-09-11 16:56:55 -0300

    Fixed awkward / lazy syntax in testing

--- src/TestSuite.java
+++ src/TestSuite.java
@@ -42,59 +42,55 @@ class TestSuite {
 
 	@Test
 	void randomNavalBase_neverLowStarport() {
-		boolean shouldBe = false;
 		String noRollList = "CDEX";
 		for (int i = 0; i < noRollList.length(); i++) {
 			UWP test = new UWP();
 			test.setNavalBase(classicTravellerRules.rollForNavalBase(noRollList.charAt(i)));
-			assertEquals(shouldBe, test.getNavalBase());
+			assertFalse(test.getNavalBase());
 		}
 	}
-	
+
 	@Test
 	void randomNavalBase_neverLowScoutBase() {
-		boolean shouldBe = false;
 		String noRollList = "EX";
 		for (int i = 0; i < noRollList.length(); i++) {
 			UWP test = new UWP();
 			test.setScoutBase(classicTravellerRules.rollForScoutBase(noRollList.charAt(i)));
-			assertEquals(shouldBe, test.getScoutBase());
+			assertFalse(test.getScoutBase());
 		}
-	}	
-	
+	}
+
 	@Test
 	void randomGasGiant_neverMoreNo() {
 		boolean shouldBe = true;
 		int hasGG = 0;
 		int noGG = 0;
 		for (int i = 0; i < 999; i++) {
-			if (classicTravellerRules.rollForGasGiant() ) {
-				hasGG++;	
-			} else
-			{
+			if (classicTravellerRules.rollForGasGiant()) {
+				hasGG++;
+			} else {
 				noGG++;
 			}
 		}
-		assertEquals(shouldBe, hasGG > noGG);
-		assertEquals(shouldBe, hasGG > 1);
-		assertEquals(shouldBe, noGG > 1);		
+		assert (hasGG > noGG);
+		assert (hasGG > 1);
+		assert (noGG > 1);
 	}
-	
-	@Test 
+
+	@Test
 	void homebrewTravelZone_isValid() {
-		boolean shouldBe = true;
-		boolean shouldNotBe = false;
-		char starportCode = 'A';		
-		
+
+		char starportCode = 'A';
+
 		for (int i = 0; i < 999; i++) {
-			assertEquals(shouldNotBe, ctHomebrew.rollForTravelZone(starportCode) == TravelZones.RED);
-			assertEquals(shouldNotBe, ctHomebrew.rollForTravelZone(starportCode) == TravelZones.AMBER);
+			assertNotEquals(ctHomebrew.rollForTravelZone(starportCode), TravelZones.RED);
+			assertNotEquals(ctHomebrew.rollForTravelZone(starportCode), TravelZones.AMBER);
 		}
-		
+
 		starportCode = 'B';
 		for (int i = 0; i < 999; i++) {
-			assertEquals(shouldNotBe, ctHomebrew.rollForTravelZone(starportCode) == TravelZones.RED);
+			assertNotEquals(ctHomebrew.rollForTravelZone(starportCode), TravelZones.RED);
 		}
 	}
-	
+
 }
